8ff2e22cb1bc6b8e31a11297670ccf9a7e892d0f
Rob French <scrape@sdf.org>
2022-03-24 21:41:51 -0500

    updated class structure

--- atu100-gui.py
+++ atu100-gui.py
@@ -23,16 +23,36 @@ swrRegEx = re.compile(r"SWR=(\d+\.\d+)")
 quit = False
 
 class TunerData:
-    def __init__(self):
+    def __init__(self, ser):
+        self.__ser = ser
         self.__lock = threading.Lock()
         self.__pwr = 0.0
         self.__swr = 0.0
         self.__ind = 0.0
         self.__cap = 0.0
+        self.__pwr_hook = None
+        self.__swr_hook = None
+        self.__ind_hook = None
+        self.__cap_hook = None        
+        self.__done = False
+
+    def setHookPower(self, hook=None):
+        self.__pwr_hook = hook
+
+    def setHookSWR(self, hook=None):
+        self.__swr_hook = hook
+
+    def setHookInductance(self, hook=None):
+        self.__ind_hook = hook
+
+    def setHookCapacitance(self, hook=None):
+        self.__cap_hook = hook
 
     def setPower(self, pwr):
         with self.__lock:
             self.__pwr = pwr
+            if (self.__pwr_hook != None):
+                self.__pwr_hook(self.__pwr)
 
     def getPower(self):
         with self.__lock:
@@ -41,6 +61,8 @@ class TunerData:
     def setSWR(self, swr):
         with self.__lock:
             self.__swr = swr
+            if (self.__swr_hook != None):
+                self.__swr_hook(self.__swr)
 
     def getSWR(self):
         with self.__lock:
@@ -49,6 +71,8 @@ class TunerData:
     def setInductance(self, ind):
         with self.__lock:
             self.__ind = ind
+            if (self.__ind_hook != None):
+                self.__ind_hook(self.__ind)
     
     def getInductance(self):
         with self.__lock:
@@ -57,65 +81,63 @@ class TunerData:
     def setCapacitance(self, cap):
         with self.__lock:
             self.__cap = cap
+            if (self.__cap_hook != None):
+                self.__cap_hook(self.__cap)
 
     def getCapacitance(self):
         with self.__lock:
             return self.__cap
 
-tun = TunerData()
-ser = serial.Serial("/dev/ttyUSB0", 9600)
+    def processSerial(self):
+        while not self.__done:
+            data = self.__ser.readline().decode()
+            pwr = pwrRegEx.search(data)
+            ind = indRegEx.search(data)
+            cap = capRegEx.search(data)
+            swr = swrRegEx.search(data)	
+            if (pwr != None):
+                self.setPower(pwr.group(1))
+                print("Power: " + self.getPower())
+            if (swr != None):
+                self.setSWR(swr.group(1))
+                print("SWR: " + self.getSWR())
+            if (ind != None):
+                self.setInductance(ind.group(1))
+                print("Inductance: " + self.getInductance())
+            if (cap != None):
+                self.setCapacitance(cap.group(1))
+                print("Capacitance: " + self.getCapacitance())
 
-def read_serial_data(frame):
-    while not quit:
-        data = ser.readline().decode()
-        pwr = pwrRegEx.search(data)
-        ind = indRegEx.search(data)
-        cap = capRegEx.search(data)
-        swr = swrRegEx.search(data)	
-        if (pwr != None):
-            print("Power: " + pwr.group(1))
-            tun.setPower(pwr.group(1))
-            frame.pwrText.SetValue("PWR = " + tun.getPower() + " W")
-        if (swr != None):
-            print("SWR: " + swr.group(1))
-            tun.setSWR(swr.group(1))
-            frame.swrText.SetValue("SWR = " + tun.getSWR())
-        if (ind != None):
-            print("Inductance: " + ind.group(1))
-            tun.setInductance(ind.group(1))
-            frame.indText.SetValue("L = " + tun.getInductance() + " uH")
-        if (cap != None):
-            print("Capacitance: " + cap.group(1))
-            tun.setCapacitance(cap.group(1))
-            frame.capText.SetValue("C = " + tun.getCapacitance() + " pF")
-
-command_table = {
-    'p': tun.getPower,
-    's': tun.getSWR,
-    'c': tun.getCapacitance,
-    'i': tun.getInductance,
-}
-
-def handle_command(cmd, action):
-    action(command_table[cmd.strip()]())
-
-def handle_console(frame):
-    while not quit:
-        print("Enter a command: ")
-        handle_command(input(), print)
-        print(frame.pwrText.GetValue())
-
-def OnClose():
-    quit = True
+#command_table = {
+#    'p': tun.getPower,
+#    's': tun.getSWR,
+#    'c': tun.getCapacitance,
+#    'i': tun.getInductance,
+#}
+#
+#def handle_command(cmd, action):
+#    action(command_table[cmd.strip()]())
+#
+#def handle_console(frame):
+#    while not quit:
+#        print("Enter a command: ")
+#        handle_command(input(), print)
+#        print(frame.pwrText.GetValue())
 
 if __name__ == "__main__":
     app = TunerApp(0)
 
-    meter = threading.Thread(target=lambda:read_serial_data(app.frame))
+    tun = TunerData(serial.Serial("/dev/ttyUSB0", 9600))
+    tun.setHookPower(lambda x:app.frame.pwrText.SetValue("PWR = " + x + " W"))
+    tun.setHookSWR(lambda x:app.frame.swrText.SetValue("SWR = " + x))
+    tun.setHookInductance(lambda x:app.frame.indText.SetValue("L = " + x + " uH"))
+    tun.setHookCapacitance(lambda x:app.frame.capText.SetValue("C = " + x + " pF"))
+
+    meter = threading.Thread(target=tun.processSerial)
     meter.start()
 
-    console = threading.Thread(target=lambda:handle_console(app.frame))
-    console.start()
+    #console = threading.Thread(target=lambda:handle_console(app.frame))
+    #console.start()
 
     app.MainLoop()
 
@@ -124,5 +146,5 @@ if __name__ == "__main__":
     #top.Show()
     #app.MainLoop()
 
-    console.join()
+    #console.join()
     meter.join()
